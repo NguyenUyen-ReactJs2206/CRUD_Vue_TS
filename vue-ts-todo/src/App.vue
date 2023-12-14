12624720dfbb9f3d43484dae9e8bb9e7e00013a4
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-12-14 23:03:25 +0700

    create todo and checked box

---
+++ vue-ts-todo/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>My Todo List</h1>
+    <h3>{{ completedAll }}</h3>
+    <ItemList :listItem="listItem" />
+    <!-- @changeItem="completedItem"  -->
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, reactive } from "vue";
+import ItemList from "./components/items/Item.vue";
+import { ItemInterface } from "./models/types/item.type";
+//Create data item
+const listItem: ItemInterface[] = reactive([
+  {
+    id: 1,
+    name: "Coffee ☕",
+    completed: true,
+  },
+  {
+    id: 2,
+    name: "Read Book 📖",
+    completed: false,
+  },
+  { id: 3, name: "Sport 🤾‍♂️", completed: false },
+  { id: 4, name: "Listening to music", completed: true },
+]);
+
+// const completedItem = (id: number) => {
+//   alert("Id Item" + id);
+// };
+
+//watch item when change
+const completedAll = computed(() => {
+  let all = listItem.length;
+  let countCompleted = 0;
+
+  listItem.forEach((item) => {
+    if (item.completed) {
+      countCompleted++;
+    }
+  });
+
+  return countCompleted === all
+    ? "[ All todo is completed. ]"
+    : "[ All todo isn't completed. ]";
+});
+</script>
---
+++ vue-ts-todo/src/components/items/Item.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>List Item</div>
+  <!--Show Item-->
+  <ul>
+    <li v-for="item in listItem" :key="item.id">
+      <input type="checkbox" value="" v-model="item.completed" />
+      <!-- @click="completedItem(item.id)" -->
+      <label for=""> {{ item.name }}</label>
+    </li>
+  </ul>
+  <div>{{ listItem }}</div>
+</template>
+
+<script setup lang="ts">
+import { ItemInterface } from "../../models/types/item.type";
+
+defineProps<{ listItem: ItemInterface[] }>();
+
+//USING DEFINE EMIT
+// const emit = defineEmits<{ (e: "changeItem", id: number): any }>();
+
+// const completedItem = (id: number) => {
+//   emit("changeItem", id);
+// };
+</script>
