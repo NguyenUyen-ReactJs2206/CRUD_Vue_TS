8f0a4dd2e465ab05dd1fbab944c3566c5e0f0014
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-12-15 15:53:01 +0700

    setup

---
+++ Vue-Crud-Ts/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="header">
+    <nav class="header__nav header__container">
+      <div class="header__branding">
+        <img src="../assets/images/Vue_Logo_Black-9190692d.png" alt="" />
+        <h1>Vue Todos</h1>
+      </div>
+      <ul class="header__menu">
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
